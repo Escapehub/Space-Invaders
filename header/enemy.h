b2ca09da22e3f9711b36f5c15359940aa4f19e53
callum <callum_speirs@hotmail.co.uk>
2020-09-26 12:03:13 +0100

    Updated enemy class

--- header/enemy.h
+++ header/enemy.h
@@ -10,12 +10,7 @@ public:
   void drawEnemies(sf::RenderWindow&, sf::Texture);
   void moveEnemies(sf::RenderWindow&);
 private:
-  //std::vector< std::vector<sf::Sprite> > enemies(12, std::vector<sf::Sprite>(6));
   void createEnemies(sf::Texture&);
-  sf::Sprite row1[12];
-  sf::Sprite row2[12];
-  sf::Sprite row3[12];
-  sf::Sprite row4[12];
-  sf::Sprite row5[12];
+  sf::Sprite enemies[12][5];
 };
 #endif // !ENEMY_H
--- source/enemy.cpp
+++ source/enemy.cpp
@@ -10,25 +10,15 @@ Enemy::~Enemy() {
 
 void Enemy::drawEnemies(sf::RenderWindow& window, sf::Texture texture) {
   createEnemies(texture);
-  for (sf::Sprite sprite : row1) {
-    window.draw(sprite);
-  }
-  for (sf::Sprite sprite : row2) {
-    window.draw(sprite);
-  }
-  for (sf::Sprite sprite : row3) {
-    window.draw(sprite);
-  }
-  for (sf::Sprite sprite : row4) {
-    window.draw(sprite);
-  }
-  for (sf::Sprite sprite : row5) {
-    window.draw(sprite);
+  for (int i = 0; i < 12; i++) {
+    for (int j = 0; j < 5; j++) {
+        window.draw(enemies[i][j]);
+    }
   }
 }
 
 void Enemy::moveEnemies(sf::RenderWindow& window) {
-  
+
 }
 
 void Enemy::createEnemies(sf::Texture& texture) {
@@ -37,26 +27,17 @@ void Enemy::createEnemies(sf::Texture& texture) {
   sf::IntRect enemy2Rect(0, 60, 60, 60);
   sf::IntRect enemy3Rect(0, 120, 60, 60);
 
+  sf::IntRect currentRect = enemy3Rect;
   for (int i = 0; i < 12; i++) {
-    // Row 1
-    row1[i].setTexture(texture);
-    row1[i].setTextureRect(enemy1Rect);
-    row1[i].setPosition(i * 60, 0);
-    // Row 2
-    row2[i].setTexture(texture);
-    row2[i].setTextureRect(enemy2Rect);
-    row2[i].setPosition(i * 60, 60);
-    // Row 3
-    row3[i].setTexture(texture);
-    row3[i].setTextureRect(enemy2Rect);
-    row3[i].setPosition(i * 60, 120);
-    // Row 4
-    row4[i].setTexture(texture);
-    row4[i].setTextureRect(enemy3Rect);
-    row4[i].setPosition(i * 60, 180);
-    // Row 5
-    row5[i].setTexture(texture);
-    row5[i].setTextureRect(enemy3Rect);
-    row5[i].setPosition(i * 60, 240);
+    for (int j = 0; j < 5; j++) {
+      if (j == 1)
+        currentRect = enemy2Rect;
+      else if (j == 3)
+        currentRect = enemy3Rect;
+
+        enemies[i][j].setTexture(texture);
+        enemies[i][j].setTextureRect(currentRect);
+        enemies[i][j].setPosition(i * 60, j * 60);
+    }
   }
 }
